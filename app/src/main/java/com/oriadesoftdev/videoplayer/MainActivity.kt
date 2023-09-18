7617d28f96a762f94d94c0e55caab2deab6453ba
EngineerSk <eskayoriade@gmail.com>
2023-09-18 15:35:34 +0100

    A basic video player implemented with VideoView and MediaController

---
+++ app/src/main/java/com/oriadesoftdev/videoplayer/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.oriadesoftdev.videoplayer
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import com.oriadesoftdev.videoplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        initVideoView()
+    }
+
+    private fun initVideoView() {
+        val mediaController = MediaController(this)
+        binding.videoView.apply {
+            mediaController.setAnchorView(this@apply)
+            setMediaController(mediaController)
+            setVideoURI(
+                Uri.parse(
+                    "android.resource://$packageName/raw/math_numeric_and_logical_operator"))
+            requestFocus()
+        }.start()
+    }
+}
